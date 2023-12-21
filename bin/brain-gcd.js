a47c1a26e0f44da731067f8f3196a4e83e9050f8
Артём Бочкарёв <chipalinchik@gmail.com>
2023-12-21 22:26:54 +0500

    step7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import AskQuestion from '../src/cli.js';
+import getRandomNumber from '../src/random.js';
+
+const Gcd = (n1, n2) => {
+  const n3 = n1 % n2;
+  if (n3 === 0) {
+    return n2;
+  }
+  const n4 = n2 % n3;
+  if (n4 === 0) {
+    return n4;
+  }
+  return Gcd(n3, n4);
+};
+
+const StartGcd = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = AskQuestion('May I have your name? ');
+  console.log(`Hello, ${name}!\nFind the greatest common divisor of given numbers.`);
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = getRandomNumber(0, 100);
+    const num2 = getRandomNumber(0, 100);
+    console.log(`Question: ${num1} ${num2}`);
+    const correct = Gcd(Math.max(num1, num2), Math.min(num1, num2));
+    const answer = AskQuestion('Your answer: ');
+    if (Number(answer) === correct) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+StartGcd();
